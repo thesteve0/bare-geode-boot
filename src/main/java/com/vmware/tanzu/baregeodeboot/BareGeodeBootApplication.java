9a1f5c94ed46a491d757f262c74d0002d2bbc34d
Steven Pousty <steve.pousty@gmail.com>
2022-04-01 21:07:13 -0700

    initial import

---
+++ src/main/java/com/vmware/tanzu/baregeodeboot/BareGeodeBootApplication.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.baregeodeboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BareGeodeBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BareGeodeBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmware/tanzu/baregeodeboot/BareGeodeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmware.tanzu.baregeodeboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BareGeodeBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
